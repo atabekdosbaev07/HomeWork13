cae97bbbb9a1d289b55a8c5839e4691fb1c83f14
atabekdosbaev07 <dosbaeatabek07@gmail.com>
2022-08-12 19:40:09 +0300

    commit1

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         Random ran = new Random();
-        int[] a = new int[5];
+        int[] a = new int[10];
         for (int i = 0; i < a.length; i++){
             a[i] = ran.nextInt(0,2);
         }
